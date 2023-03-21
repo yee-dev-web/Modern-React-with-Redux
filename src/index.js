8d414329641d292e349952e13c3ec0a3abf25d48
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-03-22 01:02:01 +0300

    Applying Styling in JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1) Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) Get a references to the div with ID root
+const el = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el)
+
+
+// 4) Create a component
+function App() {
+    // let message = 'Bye There!'
+    // let number =  Math.random()
+    // if(number < 0.5){
+    //   message = 'Hello there!'
+    // }
+
+    // const date = new Date();
+    // const time = date.toLocaleTimeString();
+    // <h1>{new Date().toLocaleTimeString()}</h1>
+
+    // const name ='Yunus';
+    // const age = 21;
+
+    // const inputType = "number"
+    // const minValue = 5;
+    // const message = 'Enter age'
+
+
+    return (
+        <div>
+            <input style={{color: "red", border: '3px solid blue'}}
+                   maxLength={6}
+            />
+
+            <input placeholder='hi there'
+                   id='ads'
+                   spellCheck
+                   type="text"
+            />
+
+
+        </div>
+    );
+}
+
+// 5) Show the compoent on the screen
+root.render(<App/>);
+
+
+
+
+
