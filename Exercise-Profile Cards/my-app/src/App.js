86de32cfbbadf195b4b34c1c356a0dffdfaa4049
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-03-23 19:12:26 +0300

    Profile card css

--- Exercise-Profile Cards/my-app/src/App.js
+++ Exercise-Profile Cards/my-app/src/App.js
@@ -9,10 +9,43 @@ import 'bulma/css/bulma.css'
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard title='Alexa' handle='@alexa99' image={AlexaImage}/>
-            <ProfileCard title='Cortana' handle='@cortana32' image={CortanaImage}/>
-            <ProfileCard title='Siri' handle='@siri01' image={SiriImage}/>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard title='Alexa'
+                                         handle='@alexa99'
+                                         image={AlexaImage}
+                                         description="Alexa was created by Amazon
+                                         and helps you buy things."
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title='Cortana'
+                                         handle='@cortana32'
+                                         image={CortanaImage}
+                                         description="Cortana was created by Google and
+                                         helps you buy things"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title='Siri'
+                                         handle='@siri01'
+                                         image={SiriImage}
+                                         description="Siri was created Apple and helps you
+                                         buy things"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+
         </div>
 
     );
