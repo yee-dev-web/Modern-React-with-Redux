67de8a411d22e96f4cfb40f590e071ad0a0c2bad
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-03-24 00:03:08 +0300

    Animals

---
+++ Exercise-Add Animal/my-app/src/AnimalShow.js
@@ -0,0 +1,9 @@
+function AnimalShow (){
+    return(
+      <div>
+          Cow!
+      </div>
+    );
+}
+
+export  default AnimalShow;
---
+++ Exercise-Add Animal/my-app/src/App.js
@@ -0,0 +1,13 @@
+import AnimalShow from "./AnimalShow";
+
+function App(){
+    return (
+          <div>
+              Show animal list here!
+              <AnimalShow/>
+          </div>
+    );
+
+}
+
+export  default App;
