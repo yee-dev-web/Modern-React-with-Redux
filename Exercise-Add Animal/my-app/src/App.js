8a0cb294b5a571634a8ce3b3de70abdf834a602a
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-03-24 16:48:30 +0300

    Add GetRandomAnimal Function and useState

--- Exercise-Add Animal/my-app/src/App.js
+++ Exercise-Add Animal/my-app/src/App.js
@@ -1,8 +1,20 @@
+import {useState} from "react";
 import AnimalShow from "./AnimalShow";
 
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse']
+
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
+
 function App() {
+    //const [count, setCount] = useState(0)
+    const [animals, setAnimals] = useState([])
+
     const handleClick = () => {
-        console.log('button was clicked!!')
+        // setCount(count + 1)
+        setAnimals([...animals, getRandomAnimal()])
     }
 
     return (
@@ -10,6 +22,8 @@ function App() {
             <button onClick={handleClick}>
                 Add Animal
             </button>
+            <div>{animals}</div>
+
         </div>
     );
 
