71c792d32f15732a4b6bcae70f06a1a8828598f3
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-03-24 17:30:42 +0300

    Include Animal Photo

--- Exercise-Add Animal/my-app/src/AnimalShow.js
+++ Exercise-Add Animal/my-app/src/AnimalShow.js
@@ -1,9 +1,25 @@
-function AnimalShow (){
-    return(
-      <div>
-          Cow!
-      </div>
+import bird from './svg/bird.svg'
+import cat from './svg/cat.svg'
+import cow from './svg/cow.svg'
+import dog from './svg/dog.svg'
+import gator from './svg/gator.svg'
+import heart from './svg/heart.svg'
+import horse from './svg/horse.svg'
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    heart,
+    horse
+};
+
+function AnimalShow({type}) {
+    return (
+        <img src={svgMap[type]} alt='animal' style={{width: '50px', height: '50px'}}/>
     );
 }
 
-export  default AnimalShow;
+export default AnimalShow;
