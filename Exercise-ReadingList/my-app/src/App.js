9067b4f62923a54a6faa8ad8fed3b347afaf4ec9
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-03-31 02:14:42 +0300

    Add My API ReadingList Project

--- Exercise-ReadingList/my-app/src/App.js
+++ Exercise-ReadingList/my-app/src/App.js
@@ -1,10 +1,20 @@
 import {useState} from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios"
 
 function App() {
 
   const [books, setBooks] = useState([])
+
+  const fetchBooks = async () => {
+    const response = await axios.get('https://localhost:3002/books')
+
+    setBooks(response.data)
+  }
+
+  // DONT DO THIS
+  fetchBooks()
 
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
@@ -25,15 +35,13 @@ function App() {
     setBooks(updatedBooks)
 
   }
-  const createBook = (title) => {
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3002/books', {
+      title,
+    });
 
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 99)
-        , title
-      },
-    ]
+
+    const updatedBooks = [...books, response.data]
 
     setBooks(updatedBooks)
   };
