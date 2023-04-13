4b75c518bfaa3a8769f93b9ac99536a8d625ca99
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2023-04-13 17:03:36 +0300

    Context Object

--- Exercise-ReadingList/my-app/src/index.js
+++ Exercise-ReadingList/my-app/src/index.js
@@ -8,7 +8,7 @@ const  el = document.getElementById('root')
 const root = ReactDOM.createRoot(el)
 
 root.render(
-      <BooksContext.Provider value={7}>
+      <BooksContext.Provider value={9}>
         <App/>
       </BooksContext.Provider>
     )
